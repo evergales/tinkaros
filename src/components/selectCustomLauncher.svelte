6756e654fe5f8ecc9ddc9c6ccd547cc297f469ca
Hbarni <hajtmanszkib@gmail.com>
2023-01-31 16:11:53 +0100

    install path validation :)

--- src/components/selectCustomLauncher.svelte
+++ src/components/selectCustomLauncher.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
   import { invoke } from "@tauri-apps/api/tauri";
+  import { exists } from '@tauri-apps/api/fs';
+  import { open } from '@tauri-apps/api/dialog';
   import { config } from "../stores/config";
   import { state } from "../stores/state";
-  import { open } from '@tauri-apps/api/dialog';
   import Tooltip from "./Tooltip.svelte";
   const all = ["default", "curseforge", "prism", "custom"]
   let selected: string;
   let customSelected: string
   let path: string | string[];
+  let validPath: boolean = true;
   let launchers: { name: string; path: string; }[];
 
   function select(e: MouseEvent & { currentTarget: EventTarget & HTMLButtonElement; }) {
@@ -57,6 +59,17 @@
     }
     launchers = known
   }
+
+  async function valid_path() {
+    if (Array.isArray(path)) {path = path[0]}
+    let known = await exists(path)
+    if (!known && path != "") {
+      validPath = false
+    } else {
+      validPath = true
+    }
+  }
+
   get_known()
 </script>
 
@@ -72,7 +85,7 @@
   {:else if selected == "custom"}
     <div class="customDiv bottomEl">
       <li style="list-style: none;">
-        <i><input bind:value={path} type="text" placeholder="custom install location"></i>
+        <i><input bind:value={path} on:change={valid_path} type="text" placeholder="custom install location"></i>
         <i><button class="selectPath" on:click={selectPath}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M64 480H448c35.3 0 64-28.7 64-64V160c0-35.3-28.7-64-64-64H298.5c-17 0-33.3-6.7-45.3-18.7L226.7 50.7c-12-12-28.3-18.7-45.3-18.7H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64z"/></svg></button></i>
       </li>
       <select bind:value={customSelected}>
@@ -80,7 +93,12 @@
         <option value="curseforge">curseforge</option>
         <option value="prism">prism</option>
       </select>
-      <button on:click={(e) => init(e)} class="customConfirm">confirm</button>
+      {#if validPath}
+      <button on:click={(e) => init(e)} class="customConfirm">confirm</button>  
+      {:else}
+      <Tooltip tip="install location invalid!" top><button class="customConfirm" style="transform: translateX(0%);" disabled>confirm</button></Tooltip>
+      {/if}
+      
     </div>
   {/if}
 </main>
@@ -132,6 +150,13 @@
     width: 50%;
     transform: translateX(50%);
   }
+  .customConfirm:disabled {
+    background-color: rgb(59, 59, 59);
+    cursor: default;
+  }
+  .customConfirm:disabled:hover {
+      border-color: rgb(59, 59, 59);
+    }
   .bottomEl {
     position: absolute;
     transform: translateX(-50%) translateY(-50%);
